9d19db0dfce4f2b32c1ff456f8e0a61248bcaae2
rose-line <1886792+rose-line@users.noreply.github.com>
2023-10-05 18:47:30 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# B1 Bases de la programmation - Groupe B
+
+Dépôt du programme support au module 1 : *introduction à la programmation*.
---
+++ src/main/java/fr/pgah/App.java
@@ -0,0 +1,24 @@
+package fr.pgah;
+
+public class App {
+
+  public static void main(String[] args) {
+
+    // 1. Avoir le salaire horaire
+
+    int salaire_horaire = 15;
+
+    // 2. Avoir le nombre d'heures travaillées
+
+    int heures_trav = 35;
+
+    // 3. Calcul de (salaire horaire) x (heures travaillées)
+
+    int salaire_brut = salaire_horaire * heures_trav;
+
+    // 4. Affichage du résultat
+
+    System.out.println(salaire_brut);
+  }
+
+}
